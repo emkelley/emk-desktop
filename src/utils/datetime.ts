acae4ade522f793fea2792b04a0dc4f3ad6e1604
Eric Kelley <hello@ericmkelley.com>
2022-04-02 19:48:34 -0400

    My preferred date format for clock

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -5,7 +5,7 @@ const hours = time.getHours() % 12 ? time.getHours() : 12;
 const ampm = hours >= 12 ? "PM" : "AM";
 
 let currentTime = `${lightFormat(new Date(), "h:mm")} ${ampm}`;
-let currentDate = lightFormat(new Date(), "M/d/yyyy");
+let currentDate = lightFormat(new Date(), "yyyy-MM-dd");
 
 const getCurrentTime = () => currentTime;
 const getCurrentDate = () => currentDate;
