6b6c835f75bdca598d24079a37f079a7ccb17387
Eric Kelley <hello@ericmkelley.com>
2022-04-03 12:44:35 -0400

    fix icon

--- src/components/webpage/Rick.vue
+++ src/components/webpage/Rick.vue
@@ -5,7 +5,7 @@ import DragWindow from "../global/DragWindowBrowser.vue";
 <template>
   <drag-window src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1">
     <template v-slot:header>
-      <i class="fa-duotone fa-internet mr-2"></i>
+      <i class="fab fa-youtube mr-2"></i>
       Rick Astley - Never Gonna Give You Up (Official Music Video) - youtube.com
     </template>
   </drag-window>
