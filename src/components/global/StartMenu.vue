b5298df7fdcd17533d1fe6e63ec25c97e6763bbc
Eric Kelley <hello@ericmkelley.com>
2022-04-03 23:20:27 -0400

    placeholder layout idea

--- src/components/global/StartMenu.vue
+++ src/components/global/StartMenu.vue
@@ -1,7 +1,9 @@
+<script lang="ts" setup></script>
+
 <template>
   <section
     id="start"
-    class="absolute bottom-[40px] w-96 bg-slate-300 bg-opacity-90 z-99 rounded-tr-lg shadow-xl flex flex-row"
+    class="absolute bottom-[40px] min-w-96 bg-slate-300 bg-opacity-90 z-99 rounded-tr-lg shadow-xl flex flex-row"
   >
     <section class="flex flex-col p-4">
       <div class="flex-grow"></div>
@@ -16,25 +18,30 @@
       </div>
     </section>
 
-    <section class="p-2">
-      <p class="text-sm">Recently installed</p>
+    <section class="p-4">
+      <p class="text-sm pb-2">Documents</p>
       <ul>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
-        <li>program name</li>
+        <li class="font-bold border-l-4 pl-2 border-indigo-400">
+          Recent Projects
+        </li>
+        <li>Who I've Done Work For</li>
+        <li>Samurai Zero</li>
+        <li>Games</li>
+        <li>Welcome.txt</li>
+        <li>AboutMe.txt</li>
+        <li>FollowMe.txt</li>
       </ul>
     </section>
 
-    <section></section>
+    <section class="p-4">
+      <p class="text-sm pb-2">Recent Projects</p>
+      <ul>
+        <li>Healthy Michigan Dental</li>
+        <li>VIZOR Scoreboards</li>
+        <li>Samurai Zero</li>
+        <li>Midnight Synth</li>
+      </ul>
+    </section>
   </section>
 </template>
-
-<script lang="ts" setup></script>
-
 <style lang="scss" scoped></style>
